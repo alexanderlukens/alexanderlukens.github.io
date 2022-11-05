bfbab8197177daab79edb8ca194a5f9d1fb5f063
Alexander Lukens <alexander.lukens1@gmail.com>
2022-11-05 15:49:51 -0700

    GH-PAGES - Updates

---
+++ precache-manifest.cd71978f3da7b59045f3fb622f68cd67.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6cf9adca12b5ccfd13703b2af4f5f3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70769ea5e3a95543662f",
+    "url": "/static/css/main.015e39d6.chunk.css"
+  },
+  {
+    "revision": "e18bc133219f54d39dfb",
+    "url": "/static/js/2.53ad6d1d.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/static/js/2.53ad6d1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70769ea5e3a95543662f",
+    "url": "/static/js/main.6fff8f83.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "7c8e5f50725885d51b72b45164274e41",
+    "url": "/static/media/avatar.7c8e5f50.png"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "54fd423489a1937caf8950d12c8a8526",
+    "url": "/static/media/html-css-javascript-lg.54fd4234.jpg"
+  },
+  {
+    "revision": "2c7701aae07adecdb0a7e336dff08def",
+    "url": "/static/media/html-css-javascript.2c7701aa.jpg"
+  },
+  {
+    "revision": "730958a228d26dee011c5aa0aa3c3db9",
+    "url": "/static/media/javascript-fullstack.730958a2.jpg"
+  },
+  {
+    "revision": "90616a5dc9acdcce7158addc89620052",
+    "url": "/static/media/mern-stack.90616a5d.jpg"
+  },
+  {
+    "revision": "a8b012a0d7afd08ee07251e1f67294ff",
+    "url": "/static/media/react-redux.a8b012a0.jpg"
+  },
+  {
+    "revision": "f370c1e0adb80194dd3ea10e468bc69a",
+    "url": "/static/media/react.f370c1e0.png"
+  }
+]);
